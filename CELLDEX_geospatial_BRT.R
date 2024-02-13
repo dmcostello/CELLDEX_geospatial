c5a0f230e46a5168fc0c2867a1bb229780499d0e
JP Schmidt <146106744+jps270@users.noreply.github.com>
2024-02-13 15:46:00 -0500

    Update CELLDEX_geospatial_BRT.R

--- CELLDEX_geospatial_BRT.R
+++ CELLDEX_geospatial_BRT.R
@@ -363,8 +363,7 @@ values<-grv2$gl_kd
 global_kd <- setValues(gr,values)
 
 # Load large lakes from glwdr to mask out lake pixels
-lakes <- glwd_load(level = 1)
-crs(lakes)<-crs(Cpts)
+lakes <- <-st_read("glwd_1.shp")
 global_kd<-mask(global_kd,lakes,inverse=T)
 
 #writeRaster(global_kd,"global_kd.tif")
